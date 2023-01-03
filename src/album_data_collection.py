163a8738ea2425052d6de97a19c36af69a7164dd
Rio <riobeggs1@gmail.com>
2023-01-04 00:33:51 +1300

    New Methods * Created methods to get albums id and name. * Created methods to get artists id and name. * Refactoring

--- src/album_data_collection.py
+++ src/album_data_collection.py
@@ -7,36 +7,69 @@ spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 class AlbumData:
     def __init__(self) -> None:
-        self._album = None
-        self._artist_name = None
+        self._album_id = None
         self._album_name = None
+        self._artist_id = None
+        self._artist_name = None
+        self._track_names = []
         self._album_cover = None
         self._release_date = None
-        self._track_names = []
         self._record_company = None
 
-    def get_album_url(self) -> None:
+    def get_album_id(self) -> None:
         """
-        Creates an album instance from the album URL given by the user.
+        Locates an albums id from the album URL given by the user.
         """
-        # Test link = "https://open.spotify.com/album/2mpzeA7pHNIDAPii4EEKsB?si=UXtfaj3ORzSSiOREq6a__A"
-        album_url = input("Album link: ")
+        # album_url = input("Album link: ")
 
-        self._album = spotify.album_tracks(album_url)["items"]
+        # Remove after testing ---------------------------------------------------------------------->
+        album_url = "https://open.spotify.com/album/2mpzeA7pHNIDAPii4EEKsB?si=UXtfaj3ORzSSiOREq6a__A"
+        # ------------------------------------------------------------------------------------------->
+        self._album_id = album_url.split("/")[-1].split("?")[0]
+
+    def get_album_name(self) -> None:
+        """
+        Queries album for albums name and stores it as a string.
+        """
+        album = spotify.album(self._album_id)
+
+        self._album_name = album["name"]
+
+    def get_artist_id(self) -> None:
+        """
+        Queries album for albums name and stores it as a string.
+        """
+        album = spotify.album(self._album_id)
+        artist = album["artists"][0]
+
+        self._artist_id = artist["id"]
+
+    def get_artist_name(self) -> None:
+        """
+        Queries album for artists name and stores it as a string.
+        """
+        album = spotify.album(self._album_id)
+        artist = album["artists"][0]
+
+        self._artist_name = artist["name"]
 
     def get_track_names(self) -> None:
         """
-        Appends each tracks name from the album to a list (self.track_names)
+        Appends each tracks name from the album to a list of strings
         """
         for track in self._album:
+
             self._track_names.append(track["name"])
 
 
 def main():
     data = AlbumData()
 
-    data.get_album_url()
-    data.get_track_names()
+    data.get_album_id()
+    data.get_album_name()
+
+    data.get_artist_id()
+    data.get_artist_name()
 
 
 if __name__ == "__main__":
